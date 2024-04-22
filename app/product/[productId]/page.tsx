ce6ef96f6b3235021fc6cc56f9c3487eb4ac4566
pranaySpark18 <evincedevapi@gmail.com>
2024-04-22 22:21:42 +0530

    product detail page loading dynamically

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,15 +1,18 @@
-import {products} from '@/utils/products';
 import { Container } from '@mui/material';
 import ProductDetails from './productDetails';
 import ListRating from './ListRating';
+import getProductById from '@/actions/getProductById';
+import NullData from '@/app/components/NullData';
 
 interface IPrams {
     productId?: string;
 }
-const Product = ({params}:{params:IPrams}) =>{
+const Product = async ({params}:{params:IPrams}) =>{
     console.log(params);
 
-    const product = products.find((item)=> item.id === params.productId)
+    const product = await getProductById(params);
+
+    if(!product) return <NullData title="Oops! Product with the given id does not exist "/>
     
     return <div className="p-8"> 
         <Container>
