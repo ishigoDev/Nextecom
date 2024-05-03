ff5917465595a6141f243952cda8dadb458a072a
pranaySpark18 <evincedevapi@gmail.com>
2024-05-03 23:20:14 +0530

    adding full code v3

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const revalidate = 0;
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
 import { truncate } from "@/utils/truncateText";
@@ -31,7 +32,7 @@ export default async function Home({searchParams}:HomeProps) {
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grids-cols-5 2xl:grid-cols-6 gap-8">
         {shuffledProducts.map((product:any,index:any)=>{
-          return <ProductCard data={product} key={index} />
+          return <ProductCard data={product} key={product.id} />
         })}
       </div>
       </Container>
