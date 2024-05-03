bbc0321b3ff49dad6126583666abfdedf1b8d84f
pranaySpark18 <evincedevapi@gmail.com>
2024-05-03 23:22:37 +0530

    adding full code v4

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -21,7 +21,7 @@ const  calculateOrderAmount = (items: CartProductType[]) =>{
 export async function POST(request: Request){
     const currentUser = await getCurrentUser()
 
-    if(!currentUser) return NextResponse.json({error:'Unauthorized'},{status:401});
+    if(!currentUser) return NextResponse.error();
 
     const body = await request.json();
     const {items,payment_intent_id} = body;
@@ -39,7 +39,7 @@ export async function POST(request: Request){
                 prisma.order.update({where:{paymentIntentId:payment_intent_id},data:{amount:total,products:items}})
             ])
             if(!existing_order) {
-                return NextResponse.json({error:'Invalid Payment intent'},{status:400})
+                return NextResponse.error()
             }
             return NextResponse.json({paymentIntent:updated_intent})
         }
@@ -54,4 +54,6 @@ export async function POST(request: Request){
         })
         return NextResponse.json({paymentIntent})
     }
+
+    return NextResponse.error();
 }
